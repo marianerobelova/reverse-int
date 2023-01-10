d73db899a0ab98a822b065397ac48ae5ea7a15df
marianerobelova <marianerobelova@gmail.com>
2023-01-10 21:37:25 +0300

    init: task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const stringN = String(Math.abs(n));
+    const reversedString = stringN.split('').reverse().join('');
+    return Number(reversedString)
 }
